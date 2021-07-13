53dd207e171ad5608693f772bd97644a3cafe5a7
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-07-13 02:34:50 -0400

    move all input related methods to ChessGameInput class

---
+++ lib/chessgame_input.rb
@@ -0,0 +1,63 @@
+require_relative './board'
+
+class ChessGameInput
+  @@BOARD_RANK = ('a'..'h').to_a
+  @@BOARD_FILE = ('1'..'8').to_a
+
+  def verify_input(min, max, input)
+    return input if input.between?(min, max)
+  end
+
+  def player_input(min, max)
+    loop do
+      user_input = gets.chomp.to_i
+      verified_number = verify_input(min, max, user_input)
+      return verified_number if !verified_number.nil?
+
+      puts "Input error! Please enter a number between #{min} and #{max}."
+    end
+  end
+
+  def verify_pos_input(input)
+    return convert_coor(input) if @@BOARD_RANK.include?(input[0].downcase) && @@BOARD_FILE.include?(input[1])
+  end
+
+  def verify_player_piece(piece_pos, current_turn)
+    player_piece = @chess_board.find_node(piece_pos).piece if !piece_pos.nil?
+    return player_piece if !player_piece.nil? && player_piece.color == current_turn
+  end
+
+  def player_piece_input
+    loop do
+      user_input = gets.chomp
+      verified_input = verify_pos_input(user_input)
+      verified_piece = verify_player_piece(verified_input)
+      return verified_piece if !verified_piece.nil?
+
+      puts "Input error! Check that your entered position is correct."
+    end
+  end
+
+  def verify_move_input(player_piece, new_pos, board)
+    return new_pos if !new_pos.nil? && player_piece.valid_move?(new_pos, board)
+  end
+
+  def player_move_input
+    loop do
+      user_input = gets.chomp
+      verified_input = verify_pos_input(user_input)
+      return verified_input if !verified_input.nil? 
+
+      puts "Input error! This move is not valid."
+    end
+  end
+
+  def convert_coor(chess_coor)
+    coor = []
+    chess_coor = chess_coor.split('')
+    coor.push(@@BOARD_RANK.find_index(chess_coor[0]))
+    coor.push(@@BOARD_FILE.find_index(chess_coor[1]))
+
+    coor
+  end
+end

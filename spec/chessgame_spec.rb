ab24df2d4b88a33e2d8e0996ea00374127efaaaa
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-07-15 15:47:56 -0400

    rename game_move to game in chessgame_spec

--- spec/chessgame_spec.rb
+++ spec/chessgame_spec.rb
@@ -4,55 +4,55 @@ require_relative '../lib/chessgame'
 require_relative '../lib/chessgame_input'
 
 describe ChessGame do
-  subject(:game_move) { described_class.new }
-  let(:chess_board) { game_move.instance_variable_get(:@chess_board) }
+  subject(:game) { described_class.new }
+  let(:chess_board) { game.instance_variable_get(:@chess_board) }
 
   describe '#get_move' do
     let(:player_piece) { chess_board.find_node([2, 1]).piece }
 
     context 'when the move is valid' do
       before do
-        game_move.send(:set_board)
+        game.send(:set_board)
       end
 
       it 'returns the new_pos' do
         new_pos = [2, 2]
-        expect(game_move.get_move(new_pos, player_piece)).to eq(new_pos)
+        expect(game.get_move(new_pos, player_piece)).to eq(new_pos)
       end
 
       it 'does not output error message' do
         new_pos = [2, 2]
         error_message = 'Input error! This move is not valid.'
 
-        expect(game_move).not_to receive(:puts).with(error_message)
-        game_move.get_move(new_pos, player_piece)
+        expect(game).not_to receive(:puts).with(error_message)
+        game.get_move(new_pos, player_piece)
       end
 
       it 'moves the player piece to the new_pos node' do
         new_pos = [2, 2]
         new_pos_node = chess_board.find_node(new_pos)
 
-        expect { game_move.get_move(new_pos, player_piece) }.to change { new_pos_node.piece }.from(nil).to(player_piece)
+        expect { game.get_move(new_pos, player_piece) }.to change { new_pos_node.piece }.from(nil).to(player_piece)
       end
 
       it 'changes the player_piece pos instance variable to new_pos' do
         new_pos = [2, 2]
 
-        expect { game_move.get_move(new_pos, player_piece) }.to change { player_piece.pos }.from([2, 1]).to(new_pos)
+        expect { game.get_move(new_pos, player_piece) }.to change { player_piece.pos }.from([2, 1]).to(new_pos)
       end
 
       it 'changes the old node pos instance variable to nil' do
         new_pos = [2, 2]
         old_pos_node = chess_board.find_node(player_piece.pos)
 
-        expect { game_move.get_move(new_pos, player_piece) }.to change { old_pos_node.piece }.from(player_piece).to(nil)
+        expect { game.get_move(new_pos, player_piece) }.to change { old_pos_node.piece }.from(player_piece).to(nil)
       end
 
       context 'when the player piece is a pawn' do
         it 'changes the has_moved instance variable to true' do
           new_pos = [2, 3]
 
-          expect { game_move.get_move(new_pos, player_piece) }.to change { player_piece.has_moved }.from(false).to(true)
+          expect { game.get_move(new_pos, player_piece) }.to change { player_piece.has_moved }.from(false).to(true)
         end
       end
 

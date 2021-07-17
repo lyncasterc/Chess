246ad5ae1040779b2e170911e1de928b6b622701
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-07-17 15:01:00 -0400

    create ComputerPlayer class

---
+++ lib/computerplayer.rb
@@ -0,0 +1,34 @@
+#frozen_string_literal: true
+
+require_relative './king'
+require_relative './rook'
+require_relative './pawn'
+require_relative './bishop'
+require_relative './queen'
+require_relative './knight'
+require_relative './chesspiece'
+require_relative './board'
+require_relative './node'
+require_relative './chessgame_input'
+
+# Computer chess player class
+class ComputerPlayer
+  attr_reader :chess_board
+
+  def initialize(chess_board)
+    @chess_board = chess_board
+  end
+
+  def make_move
+    
+  end
+
+  private
+
+  def get_pieces
+    nodes = @chess_board.board.filter { |node| !node.piece.nil? && node.piece.color == 'black' }
+    pieces = nodes.collect { |node| node.piece }
+    
+    pieces
+  end
+end`
